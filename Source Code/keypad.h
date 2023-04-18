79460f434743566d7372618182f43f96cc09b79e
AhmadHazem <87472736+AhmadHazem@users.noreply.github.com>
2023-04-18 18:16:39 +0200

    fixed schematic

---
+++ Source Code/keypad.h
@@ -0,0 +1,4 @@
+#include <reg51.h>
+
+void keypad_init(void);
+unsigned char read_char(void);
--- Source Code/main.c
+++ Source Code/main.c
@@ -1,28 +1,15 @@
-#include<reg51.h>           // special function register declarations
-                            // for the intended 8051 derivative
-														
-#include"quad_7segment.h"
-sbit LED = P3^0;            // Defining LED pin
-
-void Delay(void);           // Function prototype declaration
-
-void main (void)
-{
+#include <reg51.h>  
+#include <stdlib.h>
+#include "keypad.h"
+#include "timer0.h"
+#include "quad_7segment.h"
+sbit LED = P3^0;           
+        
+void main (void) {
 	  init_7segment();
-    while(1)                // infinite loop
+		
+    while(1)                
     {
-		show_7segment(1234);
-    }
-}
-
-void Delay(void)
-{
-    int j;
-    int i;
-    for(i=0;i<10;i++)
-    {
-        for(j=0;j<10000;j++)
-        {
-        }
+			show_7segment(1234);
     }
 }
